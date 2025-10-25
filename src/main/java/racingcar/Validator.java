29e3aa0240424409965e1b170d69ff7bfeecd795
2021037156 <tmj5574@hanyang.ac.kr>
2025-10-25 23:44:16 +0900

    feat: Add validator class

---
+++ src/main/java/racingcar/Validator.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import org.assertj.core.api.ThrowableAssert;
+
+import java.util.List;
+
+public class Validator {
+
+    static final String ERROR_EMPTY_INPUT = "잘못된 자동차 이름이 입력되었습니다.";
+    static final String ERROR_LONG_NAME = "자동차 이름이 5자를 초과하였습니다.";
+
+    public static void validateInput(String input) {
+        if (input.equals("\n")) {
+            throw new IllegalArgumentException(ERROR_EMPTY_INPUT);
+        }
+    }
+
+    public static void validateCarNames(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException(ERROR_LONG_NAME);
+            }
+        }
+    }
+}

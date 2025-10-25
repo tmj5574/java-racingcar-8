75910cbe817bded5481d4d69557d02e892c839a1
2021037156 <tmj5574@hanyang.ac.kr>
2025-10-25 23:44:00 +0900

    test: Add validator test code

---
+++ src/test/java/racingcar/ValidatorTest.java
@@ -0,0 +1,45 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class ValidatorTest extends NsTest {
+
+    @Test
+    @DisplayName("공백을 입력했을 때 예외(IllegalArgumentException)가 발생합니다.")
+    void 비정삭적인_입력1() {
+        //given
+        String invalidInput = "\n";
+
+        //when & then
+        assertThatThrownBy(() -> Validator.validateInput(invalidInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("차의 이름이 5자를 초과하는 경우 예외(IllegalArgumentException)가 발생합니다.")
+    void 비정상적인_입력2() {
+        //given
+        List<String> carNameList = List.of("abcdef");
+
+        //when & then
+        assertThatThrownBy(() -> Validator.validateCarNames(carNameList))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void setSystemIn(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}

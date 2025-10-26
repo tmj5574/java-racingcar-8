c569d10122ffd09eb960f18a2a7f5cd3cffdbc50
2021037156 <tmj5574@hanyang.ac.kr>
2025-10-26 22:04:08 +0900

    test: Add game count validator test code

--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -3,6 +3,9 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -12,7 +15,7 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 public class ValidatorTest extends NsTest {
 
     @Test
-    @DisplayName("공백을 입력했을 때 예외(IllegalArgumentException)가 발생합니다.")
+    @DisplayName("차의 이름에 공백을 입력했을 때 예외(IllegalArgumentException)가 발생합니다.")
     void 비정삭적인_입력1() {
         //given
         String invalidInput = "\n";
@@ -32,6 +35,20 @@ public class ValidatorTest extends NsTest {
         assertThatThrownBy(() -> Validator.validateCarNameCount(carNameList))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    // @Test 대신 @ParameterizedTest 사용
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "/", "f", "1f"}) // 공백, 여러 공백, 0, 문자, 숫자+문자
+    @NullAndEmptySource
+    @DisplayName("게임 횟수에 공백을 입력했을 때 예외(IllegalArgumentException)가 발생합니다.")
+    void 비정상적인_입력3(String invalidInput) {
+
+        //when & then
+        assertThatThrownBy(() -> Validator.validateTryCount(invalidInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 
     @Override
     protected void runMain() {
